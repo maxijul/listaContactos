271022402fd791187db76f797d82a8a2795eab45
maxijul <maxijul_14@hotmail.com>
2022-09-02 21:38:53 -0300

    Create user and Add Login

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,10 +4,19 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     phone_number VARCHAR(255)
 );
+
+INSERT INTO users (name, email, password) VALUES ("test", "test@test.com", "123");
 
 INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "123456789");
